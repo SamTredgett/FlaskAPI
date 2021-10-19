6af99cf9bbfe6c9772e0030d281784f7088076c9
samtredgett <sam.tredgett@gmail.com>
2021-10-19 23:25:45 +0100

    Added logic for the insurance costs and fixed database structure.
    Got most of the concepts figured out for querying for objects but was running into issues with making the queries play nice with the return values I wanted.
    
    Next steps are to get to the bottom  of the issues with my queries that are causing issues with the database.
    
    Then moving on to front-end.

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ from flask import Flask, render_template, request, jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy 
 from flask_restful import Resource, Api, reqparse
 from flask_marshmallow import Marshmallow
+from sqlalchemy import DateTime
+from sqlalchemy.sql import func
 from models import Sender,Recipient, Order
 from pprint import pprint
 import os
@@ -37,6 +39,25 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 #  Init db
 db = SQLAlchemy(app)
 
+class OrderDb(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    sender = db.Column(db.Integer,db.ForeignKey('senderDb.id'))
+    recipient = db.Column(db.Integer, db.ForeignKey('recipientDb.id'))
+    value = db.Column(db.Float, nullable=False)
+    despatch_date = db.Column(db.String, nullable=False)
+    contents_declaration = db.Column(db.String, nullable=False)
+    insurance_required = db.Column(db.Boolean, nullable=False)
+    tracking_reference = db.Column(db.String, nullable=False, unique=True)
+
+    #  Holding on to this in case i messed up the key relationships
+    # sender_id = db.Column(db.Integer, db.ForeignKey('senderDb.id'),
+    #     nullable=False)
+    # recipient_id = db.Column(db.Integer, db.ForeignKey('recipientDb.id'),
+    #     nullable=False)
+    # sender = db.relationship('SenderDb', 
+    #     backref="order")
+
 #  Create database model
 class SenderDb(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -44,8 +65,8 @@ class SenderDb(db.Model):
     street_address = db.Column(db.String, nullable=False )
     city = db.Column(db.String, nullable=False)
     country_code = db.Column(db.String, nullable=False)
-    # orders = db.relationship('order', backref='owner')
-    order_id = db.column(db.Integer, db.ForeignKey('orderDb.id'))
+    orders = db.relationship(OrderDb , db.ForeignKey('orderDb.id'),backref='owner')
+    # order_id = db.column(db.Integer, db.ForeignKey('orderDb.id'))
     #  Create a function to return something when we add it 
     def __repr__(self):
         return '<name %r>' % self.name
@@ -56,24 +77,13 @@ class RecipientDb(db.Model):
     street_address = db.Column(db.String, nullable=False )
     city = db.Column(db.String, nullable=False)
     country_code = db.Column(db.String, nullable=False)
-    # order = db.relationship('order', backref='owner')
-    order_id = db.column(db.Integer, db.ForeignKey('orderDb.id'))
+    order = db.relationship(OrderDb, backref='owner')
+    # orders = db.column(db.Integer, db.ForeignKey('orderDb.id'))
     #  Create a function to return something when we add it 
     def __repr__(self):
         return '<name %r>' % self.name
 
-# class OrderDb(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
 
-#     sender = db.relationship('sent_by', backref='sender')
-#     recipient = db.relationship('received_by', backref='recipient')
-
-    # sender_id = db.Column(db.Integer, db.ForeignKey('senderDb.id'),
-    #     nullable=False)
-    # recipient_id = db.Column(db.Integer, db.ForeignKey('recipientDb.id'),
-    #     nullable=False)
-    # sender = db.relationship('SenderDb', 
-    #     backref="order")
 
 
 
@@ -87,7 +97,7 @@ def hello_world():
 def put_orders():
     main_dict = dict(request.json)
     try: 
-
+        # Validate the sender and recipient objects against the pydantic models
         sender = Sender(**main_dict['sender'])
         recipient = Recipient(**main_dict['recipient'])
         new_dict = {
@@ -96,21 +106,44 @@ def put_orders():
             "value" : main_dict['value'],
             "despatch_date": main_dict['despatch_date'],
             "contents declaration" : main_dict['contents declaration'],
-            "insurance_required" : main_dict['insurance_required'],
+            "insurance_required" : bool(main_dict['insurance_required']),
             "tracking_reference" : main_dict['tracking_reference']
         }
         print(new_dict)
+
+        #  Insurance Checks
+        # Is it too expensive for insurance and shipping?
+        if float(new_dict['value']) >= 10000:
+            return 'Item too valuable, cannot be shipped'
+        # Did they want to insure the package?
+        if new_dict['insurance_required']: 
+            # Where is it going to?
+            if new_dict['recipient']['country_code'].lower() == 'gb':
+                insurance_cost = float(new_dict['value']) * 1.01
+            if new_dict['recipient']['country_code'].lower() in ['de', 'fr', 'be', 'nl']:
+                insurance_cost = float(new_dict['value']) * 1.015
+            else:
+                insurance_cost = float(new_dict['value']) * 1.04
+
+        if insurance_cost < 9:
+            insurance_cost = 9
+            
+
         # Now that we actually have the information we need we can begin processing it 
+
+
+        #  time to query database for the order tracking reference
+        # if OrderDb.query.filter_by(tracking_reference = new_dict['tracking_reference']).all():
+        #     print('Already exists!')
+        # if new_dict['tracking_reference'] in db.Query.filter_by().all():
+        #     return "this order already exists!"
+        stringed_time =  datetime.strptime(new_dict['despatch_date'], '%y%m%d')
+        print(stringed_time)
+
+
 
         """
             Checks to make for main logic:
-            1 - is insurance present?
-                 - add price if it is depending on if: 
-                     - UK - 1% of Value
-                     - France, Germany, Netherlands, Belgium - 1.5% of value
-                     - anywhere else - 4%
-            2 - is package value over 10k? if so no insurance and cannot be added 
-            3 - is insurance charge under 9 pounds? if so make it 9
             4 - does the tracking number already exist? if so ignore it 
             5 - don't accept orders where despatch date isn't today/tomorrow?
         """
